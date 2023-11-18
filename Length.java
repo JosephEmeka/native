ce17e99fbcea7be3909377ea0eb327f96f7a1dca
Joseph <josephemekaonah@gmail.com>
2023-11-18 11:06:49 +0100

    Length.java

---
+++ Length.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Length {
+
+public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("Enter speed in m/s:");
+
+double velocity = input.nextDouble();
+
+System.out.println("Enter acceleration in m/s2");
+
+double acceleration = input.nextDouble();
+
+double 
+  
+
+}
+
+}
