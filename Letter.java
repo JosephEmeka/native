b9aab9c4ec555620f35fc4699b9effd841f4b983
Joseph <josephemekaonah@gmail.com>
2023-11-18 11:07:39 +0100

    Letter.java file

---
+++ Letter.java
@@ -0,0 +1,29 @@
+
+public class Letter{
+
+public static void main (String[] args) {
+
+
+
+System.out.println("                                                103 Yaba local governments");          
+System.out.println("                                                lagos Nigeria");
+System.out.println("");
+System.out.println("103 mushin lagos Nigeria");
+System.out.println("Ibadan Nigeria");
+System.out.println("West Africa");
+System.out.println("");
+System.out.println("                 LOVE LETTER");
+
+System.out.println("This is the body of my letter and there are so many things i love to say");
+System.out.println("Kindly be kind to me. Church is good despite the many troubles we have today");
+
+System.out.println("");
+
+
+System.out.println("This the second paragraph, I appreciate the effort you put into the class.");
+
+System.out.println("");
+System.out.println("Yours Sincerely");
+System.out.println("Joseph Emeka Onah");
+}
+}
