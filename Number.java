85352c0aefb8a3075e4db07a6dcf3d9d19b31217
Joseph <josephemekaonah@gmail.com>
2023-11-18 11:10:45 +0100

    Number.java file

---
+++ Number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Number{
+
+public static void main (String[] args) {
+
+Scanner input = new Scanner(System.in);
+
+System.out.println(" Enter interger between 1 and 10: ");
+
+double number = input.nextDouble();
+
+if (number == 1) System.out.println("one");
+if (number == 2) System.out.println("two");
+if (number == 3) System.out.println("three");
+if (number == 4) System.out.println("four");
+if (number == 5) System.out.println("five");
+if (number == 6) System.out.println("six");
+if (number == 7) System.out.println("Seven");
+if (number == 8) System.out.println("eight");
+if (number == 9) System.out.println("nine");
+if(number == 10) System.out.println("Ten");
+
+}
+}
