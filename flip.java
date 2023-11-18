092295811f86e740bc129e269b3a15cf8a1f538d
Joseph <josephemekaonah@gmail.com>
2023-11-18 09:15:05 +0100

    flip.java file

---
+++ flip.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Flip {
+
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("enter preferred integer: ");
+
+double numImput = input.nextDouble();
+ 
+if (numImput == 0){
+
+System.out.println(1);
+}
+
+
+if (numImput == 1){
+
+System.out.println(0);
+
+}
+
+}
+
+}
