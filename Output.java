3de5fa7cd07b986bb42e25ef30bc7f1bece59dc8
Joseph <josephemekaonah@gmail.com>
2023-11-20 13:01:42 +0100

    Output.java file

---
+++ Output.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Output{
+
+public static void main (String[] args) {
+
+Scanner input = new Scanner(System.in);
+
+
+System.out.printf(" ****%n ******%n*******%n ******%n ****%n");
+
+System.out.println("*");
+System.out.println("***");
+System.out.println("*****");
+System.out.println("****");
+System.out.println("**");
+
+
+System.out.print("*");
+System.out.print("***");
+System.out.print("*****");
+System.out.print("****");
+System.out.println("**");
+
+
+System.out.print("*");
+System.out.println("***");
+System.out.println("*****");
+System.out.print("****");
+System.out.println("**");
+
+
+System.out.printf("%s%n%s%n%s%n%s%n", " *", " ***", "*****", " ***", " *");
+
+}
+}
