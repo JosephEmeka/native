8b0bf11f6b2c716c1b78f8cefca96d3b10e3fa43
JosephEmeka <josephemekaonah@gmail.com>
2023-11-27 21:15:49 +0100

    Add files via upload

---
+++ Account/Account.java
@@ -0,0 +1,47 @@
+
+public class Account {
+private String name;
+private double balance; 
+
+public Account(String name, double balance){
+		this.name = name;
+		
+		this.balance = balance; 
+	
+}
+
+public void deposit(double depositAmount) { 
+		if (depositAmount > 0.0) { 
+ 		balance = balance + depositAmount; 
+
+ 		} 
+	}
+
+
+public double getBalance() {
+
+		return balance;
+	}
+
+
+
+public void setName (String name) {
+		this.name = name;
+	}
+
+public String getName () {
+
+		return name;
+
+
+	}
+
+
+public void withdraw (double withdraw) {
+
+
+		balance = balance - withdraw;
+
+		
+   }
+}
